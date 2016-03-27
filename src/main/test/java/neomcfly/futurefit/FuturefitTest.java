16d9695a9e62cbca05059beee7f257c873fae2b7
Benoit <benoit.theunissen@gmail.com>
2016-03-27 15:27:08 +0200

    test add Futurefit instance

--- src/main/test/java/neomcfly/futurefit/FuturefitTest.java
+++ src/main/test/java/neomcfly/futurefit/FuturefitTest.java
@@ -2,10 +2,23 @@ package neomcfly.futurefit;
 
 import org.junit.Test;
 
+import btheu.retrofit.jsoup.converter.JSoupConverter;
+import retrofit.RestAdapter.LogLevel;
+
 public class FuturefitTest {
 
     @Test
     public void test() {
+
+        Futurefit adapter = new Futurefit.Builder().setLogLevel(LogLevel.FULL)
+                .setEndpoint("https://www.google.com/")
+                .setConverter(new JSoupConverter()).build();
+
+        Api create = adapter.create(Api.class);
+
+    }
+
+    public static interface Api {
 
     }
 

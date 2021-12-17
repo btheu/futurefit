6227d6a5c9a214c7b9cf84f0ce56490a195dc466
Benoit Theunissen <benoit.theunissen@gmail.com>
2021-12-17 16:13:38 +0100

    fix(ehCache): DefaultCacheManagerProvider with key & value serializer

--- src/main/java/futurefit2/core/cache/DefaultCacheManagerProvider.java
+++ src/main/java/futurefit2/core/cache/DefaultCacheManagerProvider.java
@@ -8,7 +8,10 @@ import org.ehcache.config.builders.ExpiryPolicyBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
 import org.ehcache.config.units.EntryUnit;
 import org.ehcache.config.units.MemoryUnit;
+import org.ehcache.impl.serialization.PlainJavaSerializer;
 
+import futurefit2.Futurefit;
+import futurefit2.core.cache.CacheManagerInitializator.CacheDefinition;
 import futurefit2.core.cache.CacheManagerInitializator.CacheDefinitions;
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,7 +22,7 @@ public class DefaultCacheManagerProvider implements Function<CacheDefinitions, C
     public CacheManager apply(CacheDefinitions t) {
         CacheManagerBuilder<org.ehcache.CacheManager> builder = CacheManagerBuilder.newCacheManagerBuilder();
 
-        t.getDefinitions().forEach(d -> {
+        for (CacheDefinition d : t.getDefinitions()) {
             log.debug("creating cache name={}", d.getName());
 
             ResourcePoolsBuilder rpb = ResourcePoolsBuilder.newResourcePoolsBuilder();
@@ -30,12 +33,14 @@ public class DefaultCacheManagerProvider implements Function<CacheDefinitions, C
                 rpb = rpb.heap(d.getHeapSize(), EntryUnit.ENTRIES);
             }
 
-            CacheConfigurationBuilder<Object, Object> cc = CacheConfigurationBuilder
-                    .newCacheConfigurationBuilder(Object.class, Object.class, rpb)
-                    .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(d.getTimeToLive()));
+            CacheConfigurationBuilder<Object, Object> cc = //
+                    CacheConfigurationBuilder.newCacheConfigurationBuilder(Object.class, Object.class, rpb) //
+                            .withKeySerializer(new PlainJavaSerializer<Object>(Futurefit.class.getClassLoader())) //
+                            .withValueSerializer(new PlainJavaSerializer<Object>(Futurefit.class.getClassLoader())) //
+                            .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(d.getTimeToLive()));
 
-            builder.withCache(d.getName(), cc);
-        });
+            builder = builder.withCache(d.getName(), cc);
+        }
 
         org.ehcache.CacheManager cacheManager = builder.build();
 
